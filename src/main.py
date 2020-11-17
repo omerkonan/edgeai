45b16f5ab020c1bbc8795002c725acf1a2f91836
Ömer Konan <konanomer@gmail.com>
2020-11-17 16:06:48 +0300

    convNetwork added

--- src/main.py
+++ src/main.py
@@ -4,8 +4,30 @@ import tensorflow as tf
 import numpy as np
 from tensorflow import keras
 from sklearn import preprocessing
+from sklearn.metrics import confusion_matrix
 
 
+
+def convNetwork():
+	model = keras.Sequential([
+						keras.layers.Conv1D(filters=64, kernel_size=2, activation='relu', input_shape=(3,1)),\
+						keras.layers.Conv1D(filters=64, kernel_size=2, activation='relu'),\
+						keras.layers.Dropout(0.5),\
+						keras.layers.MaxPooling1D(pool_size=1),\
+						keras.layers.Flatten(),\
+						keras.layers.Dense(100, activation='relu'),\
+						keras.layers.Dense(4, activation='softmax')])
+	return model
+
+def deepNNetwork(label_list):
+	model = keras.Sequential(([keras.layers.Dense(20,activation=tf.nn.relu, input_shape=(3,)),\
+						keras.layers.Dense(100, activation=tf.nn.relu),\
+						keras.layers.Dense(100, activation=tf.nn.relu),\
+						keras.layers.Dense(50, activation=tf.nn.relu),\
+						keras.layers.Dense(20, activation=tf.nn.relu),\
+						keras.layers.Flatten(),
+						keras.layers.Dense(len(label_list), activation=tf.nn.softmax)]))
+	return model
 
 def main():
 
@@ -17,29 +39,32 @@ def main():
 	test_rate = 0.2
 	val_rate = 0.2
 	data = Data(datapath, headers, seperator, label_list, test_rate, val_rate)
-	print(data.x_train)
-	model = keras.Sequential(([keras.layers.Dense(20,activation=tf.nn.relu, input_shape=(3,)),\
-						keras.layers.Dense(100, activation=tf.nn.relu),\
-						keras.layers.Dense(100, activation=tf.nn.relu),\
-						keras.layers.Dense(50, activation=tf.nn.relu),\
-						keras.layers.Dense(20, activation=tf.nn.relu),\
-						keras.layers.Flatten(),
-						keras.layers.Dense(len(label_list), activation=tf.nn.softmax)]))
 	
-	model.compile(optimizer ='sgd', loss=keras.losses.sparse_categorical_crossentropy, metrics=['accuracy'])
+	cnn_x_train, cnn_y_train, cnn_x_test, cnn_y_test,\
+		cnn_x_val, cnn_y_val, = data.convertConv1D()
+	
+	checkpoints = keras.callbacks.ModelCheckpoint("../models/weights.{epoch:02d}-{loss:.2f}.hdf5", \
+													monitor='loss', save_best_only=False, period=10)
+
+	#model = convNetwork()
+	model = tf.keras.models.load_model("../models/weights.100-0.35.hdf5")
+	#model.compile(optimizer ='adam', loss="sparse_categorical_crossentropy", metrics=['accuracy'])
 	model.summary()
-	print(data.y_val)
-	print(data.x_val)
 
-	model.fit(data.x_train,data.y_train,epochs=100)
+	#model.fit(cnn_x_train,cnn_y_train,epochs=100, callbacks=[checkpoints])
 	print("validation")
-	score = model.evaluate(data.x_val, data.y_val)
-	print(f'Test loss: {score[0]} / Test accuracy: {score[1]}')
-	model.predict()
-	
+	#score = model.evaluate(cnn_x_val, cnn_y_val)
+	#print(f'Test loss: {score[0]} / Test accuracy: {score[1]}')
+	print("Test solutions:")
+	solutions = model.predict(cnn_x_test)
+	print(solutions.argmax)
+	print(solutions.argmax(axis=-1))
+	print("cnn_y_test:")
+	print(cnn_y_test)
 
-
-
+	cm = confusion_matrix(cnn_y_test,solutions.argmax(axis=-1)) 
+	print(cm)
+	#model.save("../models/model")
 
 
 
